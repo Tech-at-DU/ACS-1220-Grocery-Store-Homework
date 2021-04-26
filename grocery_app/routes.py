fc46bfe4001cc779b11d923710c84758e16bf6c9
meredithcat <meredithcat@gmail.com>
2021-04-26 03:54:57 -0400

    Update routes.py

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
 from grocery_app.models import GroceryStore, GroceryItem
-# from grocery_app.forms import BookForm, AuthorForm, GenreForm
 
 # Import app and db from events_app package so that we can run app
 from grocery_app import app, db
